c8535d333ffcac7fd5c5dd63abbb7df13833b883
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-29 21:04:06 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,30 @@
 class Artist
+song_count = 0
   attr_accessor :name, :songs
-
-
-
-
   def initialize(name)
     @name = name
-    @songs = []
-
   end
-
-
-
+  def songs
+    Song.all.select do |song|
+        song.artist == self
+  end
 end
+def add_song(song)
+song.artist = self
+end
+def add_song_by_name(name)
+artist = Song.new(name)
+add_song(artist)
+end
+def self.song_count
+  Song.all.count
+end
+end
+
+
+
+
+
 
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,19 @@
 class Song
-
-
+  attr_accessor :artist, :name
+  @@all = []
+def initialize(name)
+  @name = name
+  @@all << self
+  @songs = []
+end
+ def self.all
+   @@all
+end
+def artist_name
+if artist
+  self.artist.name
+else
+  nil
+end
+end
 end
