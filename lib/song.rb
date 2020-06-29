db1c0b3559f681d115b8e7fea84f3afe7b0fe043
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-06-29 18:53:57 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,22 @@
-class Song 
-  attr_accessor :name, :artist 
-  
+class Song
+  attr_accessor :artist, :name, :genre
+ 
+  @@all = []
   def initialize(name)
-    @name = name 
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
   end 
-end 
+  
+  def artist_name
+    if artist == nil
+      nil
+    else
+      self.artist.name
+    end
+  end
+  
+end
