20be56ebd176cc050bd7bd1b157b8b04a1f83051
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-29 17:45:55 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -6,6 +6,23 @@ class Author
     @name = name 
   end 
   
+  def posts
+    Post.all.select do |post|
+      post.author == self
+    end 
+  end 
   
+  def add_post(post)
+   post.author = self 
+  end 
+  
+  def add_post_by_title(post_title)
+    writer = Post.new(post_title)
+    add_post(writer)
+  end 
+  
+  def self.post_count
+    Post.all.count
+  end 
 
 end 
--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,16 @@
 class Post 
   attr_accessor :author, :title 
 
+  @@all = [] 
+  
   def initialize(title)
-    @title = title 
-  end
+    @title = title
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
   
   def author_name
     if author
