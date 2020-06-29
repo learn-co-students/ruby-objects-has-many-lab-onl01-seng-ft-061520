3638e6ca5cd2b103deffacb9c705d207d7275ba5
Jessica Pereira <jcpereira070@gmail.com>
2020-06-29 20:21:26 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist
+  attr_accessor :name, :songs
+  def initialize(name)
+    @name = name
+  end
+  def songs
+    Song.all.select do |song|
+        song.artist == self
+end
+def add_song(song)
+song.artist = self
+@songs << song
+  end
+end
+end
